7c96440e2701ee1b575458cd8fb3112225ef638a
mirelamoraess <mirela.moraes02@gmail.com>
2024-07-22 09:01:35 -0300

    Atualizado

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Importação das bibliotecas
-from sklearn.datasets import load_breast_cancer # Funções para carregar datasets
-from sklearn import svm  # Módulo SVM (Support Vector Machine) para classificação e regressão
-from sklearn.model_selection import train_test_split  # Função para dividir dados em treino e teste
-import matplotlib.pyplot as plt  # Biblioteca para plotagem de gráficos
-
+from sklearn.datasets import load_breast_cancer  
+from sklearn import svm 
+from sklearn.model_selection import train_test_split  
+import matplotlib.pyplot as plt 
 
 # Carregando os datasets
 dataset_cancer = load_breast_cancer()
@@ -12,33 +11,31 @@ dataset_cancer = load_breast_cancer()
 print(dataset_cancer.feature_names)
 print(dataset_cancer.target_names)
 
-
 # Separando os dados em conjuntos de treino e teste
-X_train_can, X_test_can, y_train_can, y_test_can = train_test_split(
+X_treino_can, X_teste_can, y_treino_can, y_teste_can = train_test_split(
    dataset_cancer.data, dataset_cancer.target, stratify=dataset_cancer.target, random_state=42)
 # train_test_split divide o dataset em dados de treino e teste,
 # stratify mantém a mesma proporção de classes nos dados de treino e teste,
 # random_state garante a reprodutibilidade da divisão
 
-# Lista para armazenar as métricas de acurácia
-training_accuracy = []
-test_accuracy = []
+# Lista para armazenar as métricas de precisão
+precisao_treino = []
+precisao_teste = []
 
 # Lista de kernels para SVM
 kernels = ['linear', 'rbf', 'sigmoid']
 
 # Loop sobre os diferentes kernels
 for kernel in kernels:
-   svm_model = svm.SVC(kernel=kernel)  # Cria um modelo SVM com o kernel atual
-   svm_model.fit(X_train_can, y_train_can)  # Treina o modelo SVM com os dados de treino
-   training_accuracy.append(svm_model.score(X_train_can, y_train_can))  # Calcula a acurácia no treino e armazena
-   test_accuracy.append(svm_model.score(X_test_can, y_test_can))  # Calcula a acurácia no teste e armazena
+   modelo_svm = svm.SVC(kernel=kernel)  # Cria um modelo SVM com o kernel atual
+   modelo_svm.fit(X_treino_can, y_treino_can)  # Treina o modelo SVM com os dados de treino
+   precisao_treino.append(modelo_svm.score(X_treino_can, y_treino_can))  # Calcula a precisão no treino e armazena
+   precisao_teste.append(modelo_svm.score(X_teste_can, y_teste_can))  # Calcula a precisão no teste e armazena
    
-   
-# Plotagem dos resultados
-plt.plot(kernels, training_accuracy, label='Acurácia no conj. treino')  # Plota acurácia de treino
-plt.plot(kernels, test_accuracy, label='Acurácia no conj. teste')  # Plota acurácia de teste
-plt.ylabel('Acurácia')  # Rótulo do eixo y
-plt.xlabel('Kernels')  # Rótulo do eixo x
-plt.legend()  # Adiciona legenda
-plt.show()  # Exibe o gráfico
+# Geração dos resultados
+plt.plot(kernels, precisao_treino, label='Precisão no conj. treino')  # Gera gráfico de precisão de treino
+plt.plot(kernels, precisao_teste, label='Precisão no conj. teste')  # Gera gráfico de precisão de teste
+plt.ylabel('Precisão')
+plt.xlabel('Kernels') 
+plt.legend()  
+plt.show()  
